dd7f7e122c4c5a01b87641c5af8b0538fb2bb8fb
Carlos Magalhaes <carlos.magalhaes@inatel.br>
2023-03-02 14:35:55 -0300

    Adicionado MGET para listar todos os médicos apartir de uma DTO/record

--- src/main/java/br/whitefox/project/digitalmed/controller/MedicoController.java
+++ src/main/java/br/whitefox/project/digitalmed/controller/MedicoController.java
@@ -1,16 +1,16 @@
 package br.whitefox.project.digitalmed.controller;
 
 import br.whitefox.project.digitalmed.medico.DadosCadastroMedico;
+import br.whitefox.project.digitalmed.medico.DadosListagemMedico;
 import br.whitefox.project.digitalmed.medico.Medico;
 import br.whitefox.project.digitalmed.medico.MedicoRepository;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -18,9 +18,23 @@ public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
 
+    /**
+     * Cadastra um novo médico passando informações pelo corpo
+     * @param dados
+     */
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         medicoRepository.save(new Medico(dados));
+    }
+
+    /**
+     * Traz apenas as informações que quero do DTO/record e não todas que o médico possui
+     * Assim eu passo o DTO em List<>. a linha do return converte para uma lista o DTO.
+     * @return Lista de médicos
+     */
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
--- src/main/java/br/whitefox/project/digitalmed/controller/PacienteController.java
+++ src/main/java/br/whitefox/project/digitalmed/controller/PacienteController.java
@@ -5,6 +5,7 @@ import br.whitefox.project.digitalmed.paciente.Paciente;
 import br.whitefox.project.digitalmed.paciente.PacienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class PacienteController {
     private PacienteRepository pacienteRepository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
         pacienteRepository.save(new Paciente(dados));
     }
